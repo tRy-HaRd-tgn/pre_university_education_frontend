345afc6849488b3d9deb47d5919efae6b26f3889
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-04-20 11:49:14 +0300

    feat: add some files feat: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+        fontSize: "150%",
+        flexDirection: "column",
+      }}
+    >
+      404
+      <p>данная страница не найдена</p>
+    </div>
+  );
+}
