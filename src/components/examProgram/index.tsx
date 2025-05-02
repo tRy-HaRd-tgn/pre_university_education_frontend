cb572dc2431b36bdab9119f99f61da0e4036a172
vyantik <akarizidu2@gmail.com>
2025-05-02 16:31:51 +0300

    fix: sam mistake

--- src/components/examProgram/index.tsx
+++ src/components/examProgram/index.tsx
@@ -1,60 +1,69 @@
 import styles from "./styles.module.scss";
 import Image from "next/image";
 export const ExamProgram = () => {
-  return (
-    <div className={styles.container}>
-      <h2 className={styles.h2}>
-        ПРОГРАММА &nbsp; <span className={styles.span}>ОБУЧЕНИЯ</span>
-      </h2>
-      <div className={styles.wrapper}>
-        <div className={styles.left}>
-          <div className={styles.leftTop}>
-            <div className={styles.leftColor}></div>
-            <p className={styles.leftText}>
-              <span className={styles.leftTextSpan}>Структура занятий:</span>
-              <li>Теоретические лекции</li> <li>Индивидуальные консультации</li>
-              <li>Повторение материала</li>
-              <li> Проведение тестов и контрольных работ</li>
-            </p>
-          </div>
-          <div className={styles.leftBottom}>
-            <div className={styles.leftBottomText}>
-              <h2 className={styles.heading}>
-                Программа подготовки к экзаменам:
-              </h2>
-              <p className={styles.leftBottomTextP}>
-                предназначена для учеников, которые хотят повысить свои знания и
-                навыки для успешной сдачи экзаменов. Программа включает в себя
-                теоретические и практические занятия, а также индивидуальные
-                консультации с преподавателями.
-              </p>
-            </div>
-            <div className={styles.rightColor}></div>
-          </div>
-        </div>
-        <div className={styles.right}>
-          <div className={styles.rightTop}>
-            <div className={styles.rightTopColor} />
-            <p className={styles.rightTopText}>
-              <span className={styles.heading}>Цель программы</span> - помочь
-              ученикам лучше подготовиться к экзаменам и получить максимально
-              возможный балл. Результаты обучения включают в себя глубокое
-              понимание материала, улучшенные навыки решения задач и повышение
-              уверенности в своих силах.
-            </p>
-          </div>
-          <div className={styles.rightBottom}>
-            <p className={styles.rightBotomText}>О ШКОЛЕ</p>
-            <Image
-              className={styles.arrow}
-              src={"/bottomArrow.svg"}
-              alt="error"
-              width={50}
-              height={50}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.h2}>
+				ПРОГРАММА &nbsp; <span className={styles.span}>ОБУЧЕНИЯ</span>
+			</h2>
+			<div className={styles.wrapper}>
+				<div className={styles.left}>
+					<div className={styles.leftTop}>
+						<div className={styles.leftColor}></div>
+						<div className={styles.leftText}>
+							<span className={styles.leftTextSpan}>
+								Структура занятий:
+							</span>
+							<ul>
+								<li>Теоретические лекции</li>
+								<li>Индивидуальные консультации</li>
+								<li>Повторение материала</li>
+								<li>Проведение тестов и контрольных работ</li>
+							</ul>
+						</div>
+					</div>
+					<div className={styles.leftBottom}>
+						<div className={styles.leftBottomText}>
+							<h2 className={styles.heading}>
+								Программа подготовки к экзаменам:
+							</h2>
+							<p className={styles.leftBottomTextP}>
+								предназначена для учеников, которые хотят
+								повысить свои знания и навыки для успешной сдачи
+								экзаменов. Программа включает в себя
+								теоретические и практические занятия, а также
+								индивидуальные консультации с преподавателями.
+							</p>
+						</div>
+						<div className={styles.rightColor}></div>
+					</div>
+				</div>
+				<div className={styles.right}>
+					<div className={styles.rightTop}>
+						<div className={styles.rightTopColor} />
+						<p className={styles.rightTopText}>
+							<span className={styles.heading}>
+								Цель программы
+							</span>{" "}
+							- помочь ученикам лучше подготовиться к экзаменам и
+							получить максимально возможный балл. Результаты
+							обучения включают в себя глубокое понимание
+							материала, улучшенные навыки решения задач и
+							повышение уверенности в своих силах.
+						</p>
+					</div>
+					<div className={styles.rightBottom}>
+						<p className={styles.rightBotomText}>О ШКОЛЕ</p>
+						<Image
+							className={styles.arrow}
+							src={"/bottomArrow.svg"}
+							alt="error"
+							width={50}
+							height={50}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -3,25 +3,41 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-export function Map() {
-  delete (L.Icon.Default.prototype as any)._getIconUrl;
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
-  L.Icon.Default.mergeOptions({
-    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-    iconRetinaUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-  });
-  return (
-    <MapContainer
-      center={[47.20549, 38.938607]}
-      zoom={15}
-      style={{ height: "100%", width: "100%", borderRadius: "15px" }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[47.20549, 38.938607]}>
-        <Popup>Центр довузовской подготовки</Popup>
-      </Marker>
-    </MapContainer>
-  );
+const MapComponent = dynamic(
+	() => import("./MapClient").then((mod) => mod.MapClient),
+	{
+		ssr: false,
+		loading: () => (
+			<div
+				style={{
+					height: "100%",
+					width: "100%",
+					borderRadius: "15px",
+					background: "#f0f0f0",
+				}}
+			/>
+		),
+	}
+);
+
+export function Map() {
+	return (
+		<Suspense
+			fallback={
+				<div
+					style={{
+						height: "100%",
+						width: "100%",
+						borderRadius: "15px",
+						background: "#f0f0f0",
+					}}
+				/>
+			}
+		>
+			<MapComponent />
+		</Suspense>
+	);
 }
