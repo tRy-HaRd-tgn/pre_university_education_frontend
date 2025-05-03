a00f46d66259591a1b46b16d3754a8aacf5e9423
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-05-03 21:04:22 +0300

    feat: make two pages private

--- src/app/defense/page.tsx
+++ src/app/defense/page.tsx
@@ -2,12 +2,15 @@ import styles from "./styles.module.scss";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { DefenseWrapper } from "@/components/defenseWrapper";
+import { PrivateRoute } from "@/components/privateRoute";
 export default function Page() {
   return (
-    <div className={styles.container}>
-      <Header flag={true} />
-      <DefenseWrapper />
-      <Footer />
-    </div>
+    <PrivateRoute>
+      <div className={styles.container}>
+        <Header flag={true} />
+        <DefenseWrapper />
+        <Footer />
+      </div>
+    </PrivateRoute>
   );
 }
--- src/app/position/page.tsx
+++ src/app/position/page.tsx
@@ -2,12 +2,15 @@ import styles from "./styles.module.scss";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { PositionWrapper } from "@/components/positionWrapper";
+import { PrivateRoute } from "@/components/privateRoute";
 export default function Page() {
   return (
-    <div className={styles.container}>
-      <Header flag={true} />
-      <PositionWrapper />
-      <Footer />
-    </div>
+    <PrivateRoute>
+      <div className={styles.container}>
+        <Header flag={true} />
+        <PositionWrapper />
+        <Footer />
+      </div>
+    </PrivateRoute>
   );
 }
