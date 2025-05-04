f3f63aa78e1e2e9e755d6035f3fc99212f1532e7
vyantik <akarizidu2@gmail.com>
2025-05-04 21:29:03 +0300

    feat: refactor profile page to manage user data with state and improve input handling

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -12,30 +12,68 @@ import { useSelector } from "react-redux";
 import { useState } from "react";
 
 export default function Page() {
-  const [array, setArray] = useState(
-    useSelector((state: any) => state.userSlice)
-  );
-  console.log(array);
-  return (
-    <PrivateRoute>
-      <Header flag={true} />
-      <div className={styles.container}>
-        <div className={styles.leftSide}>
-          <ProfileLogo img={"/profile.png"} />
-        </div>
-        <div className={styles.rightSide}>
-          {data.map((item, index) => (
-            <Input
-              value={array[item.type]}
-              className={styles.input}
-              key={index}
-              placeholder={item.placeholder}
-            />
-          ))}
-          <ButtonsWrapper />
-        </div>
-      </div>
-      <Footer />
-    </PrivateRoute>
-  );
+	const [profile, setProfile] = useState({});
+	const [name, setName] = useState("");
+	const [surname, setSurname] = useState("");
+	const [patronymic, setPatronymic] = useState("");
+
+	const userName = useSelector((state: any) => state.userSlice.name);
+	const userSurname = useSelector((state: any) => state.userSlice.surname);
+	const userPatronymic = useSelector(
+		(state: any) => state.userSlice.patronymic
+	);
+
+	useEffect(() => {
+		setName(userName);
+		setSurname(userSurname);
+		setPatronymic(userPatronymic);
+	}, [userName, userSurname, userPatronymic]);
+
+	useEffect(() => {
+		setProfile({
+			name,
+			surname,
+			patronymic,
+		});
+	}, [name, patronymic, surname]);
+
+	const handleInputChange = (key: string, value: string) => {
+		switch (key) {
+			case "name":
+				setName(value);
+				break;
+			case "surname":
+				setSurname(value);
+				break;
+			case "patronymic":
+				setPatronymic(value);
+				break;
+		}
+	};
+
+	return (
+		<PrivateRoute>
+			<Header flag={true} />
+			<div className={styles.container}>
+				<div className={styles.leftSide}>
+					<ProfileLogo img={"/profile.png"} />
+				</div>
+				<div className={styles.rightSide}>
+					{Object.entries(profile).map(([key, value]) => (
+						<Input
+							value={value as string}
+							onChange={(e) =>
+								handleInputChange(key, e.target.value)
+							}
+							className={styles.input}
+							key={key}
+							placeholder={key}
+						/>
+					))}
+					<ButtonsWrapper />
+				</div>
+			</div>
+			<Footer />
+		</PrivateRoute>
+	);
 }
