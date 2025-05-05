72d52a6aeee0ac5324c0d2983b6d03bd8e6fe647
vyantik <akarizidu2@gmail.com>
2025-05-05 23:03:09 +0300

    feat: update global styles and add Inter font for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,43 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "react-redux";
 import { store } from "../../store";
 import { Toaster } from "sonner";
 import { StoreProvider } from "@/components/storeProvider";
 
+const inter = Inter({
+	subsets: ["latin", "cyrillic"],
+	variable: "--font-inter",
+});
+
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <Provider store={store}>
-      <html lang="ru">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <StoreProvider>{children}</StoreProvider>
-          <div id="modal-root" />
-        </body>
-      </html>
-      <Toaster />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<html lang="ru">
+				<body
+					className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
+				>
+					<StoreProvider>{children}</StoreProvider>
+					<div id="modal-root" />
+				</body>
+			</html>
+			<Toaster />
+		</Provider>
+	);
 }
