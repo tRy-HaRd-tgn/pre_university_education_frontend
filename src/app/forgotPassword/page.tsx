a339d44b3808e5d78e874a3c1efd55e479cc60d4
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-05-05 23:55:12 +0300

    feat: add password recovery

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -1,8 +1,65 @@
+"use client";
 import styles from "./styles.module.scss";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import PasswordRecoveryService from "@/service/passwordRecoveryService";
+import { useState } from "react";
 export default function Page() {
+  const router = useRouter();
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div className={styles.container}>
-      <form className={styles.form} action=""></form>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          try {
+            setError(false);
+            setIsLoading(true);
+            const response = await PasswordRecoveryService.sendRecoveryEmail(
+              email
+            );
+            setIsLoading(false);
+            setSuccess(true);
+          } catch (e) {
+            setError(true);
+            setSuccess(false);
+            setIsLoading(false);
+          }
+        }}
+        className={styles.form}
+        action=""
+      >
+        <Button
+          onClick={() => router.replace("/")}
+          className={styles.backButton}
+          type="button"
+        >
+          Назад
+        </Button>
+        <h3 className={styles.heading}>Восстановление пароля</h3>
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className={styles.input}
+          placeholder="Почта"
+          type="email"
+        />
+        {error && (
+          <p className={styles.error}>
+            Произошла ошибка, возможно email недействителен
+          </p>
+        )}
+        {success && (
+          <p className={styles.success}>Письмо отправлено на почту</p>
+        )}
+        <Button disabled={isLoading} type="submit">
+          {isLoading ? "Отправка..." : "Отправить"}
+        </Button>
+      </form>
     </div>
   );
 }
--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,29 +1,29 @@
 import $api from "../http";
 export default class AuthService {
-	static async register(
-		name: string,
-		surname: string,
-		patronymic: string,
-		email: string,
-		password: string,
-		passwordRepeat: string
-	) {
-		return $api.post("/auth/register", {
-			name,
-			surname,
-			patronymic,
-			email,
-			password,
-			passwordRepeat,
-		});
-	}
-	static async login(email: string, password: string) {
-		return $api.post("/auth/login", { email, password });
-	}
-	static async logout() {
-		return $api.post("/auth/logout");
-	}
-	static async confirmEmail(token: string) {
-		return $api.post("/auth/email-confirmation", { token });
-	}
+  static async register(
+    name: string,
+    surname: string,
+    patronymic: string,
+    email: string,
+    password: string,
+    passwordRepeat: string
+  ) {
+    return $api.post("/auth/register", {
+      name,
+      surname,
+      patronymic,
+      email,
+      password,
+      passwordRepeat,
+    });
+  }
+  static async login(email: string, password: string) {
+    return $api.post("/auth/login", { email, password });
+  }
+  static async logout() {
+    return $api.post("/auth/logout");
+  }
+  static async confirmEmail(token: string) {
+    return $api.post("/auth/email-confirmation", { token });
+  }
 }
