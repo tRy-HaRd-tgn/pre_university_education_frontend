cd5ab1f85bcec99a42871af876b7e15c59e0561e
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-05-07 14:58:29 +0300

    feat: add faq

--- src/components/questionPlacement/index.tsx
+++ src/components/questionPlacement/index.tsx
@@ -1,10 +1,27 @@
+"use client";
 import styles from "./styles.module.scss";
 import { Component } from "./component";
 import { data } from "./data";
+import { useEffect } from "react";
+import { useState } from "react";
+import FaqService from "@/service/faqService";
+import { Axios, AxiosResponse } from "axios";
 export const QuestionsPlacement = () => {
+  const [faq, setFaq] = useState([]);
+  async function getFAQ() {
+    try {
+      const response = await FaqService.getFaq();
+      setFaq(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  useEffect(() => {
+    getFAQ();
+  }, []);
   return (
     <div className={styles.container}>
-      {data.map((item, index) => {
+      {faq?.map((item: any, index: any) => {
         return (
           <Component
             key={index}
