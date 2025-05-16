f944adb66eeaa04995d3033f7e64703df2c75255
vyantik <akarizidu2@gmail.com>
2025-05-16 15:50:34 +0300

    fix: add error handling to carousel and webinar components

--- src/components/webinarPlacements/index.tsx
+++ src/components/webinarPlacements/index.tsx
@@ -4,33 +4,37 @@ import WebinarService from "@/service/webinarService";
 import { Webinar } from "./webinar";
 
 export const WebinarPlacements = async () => {
-  const response = await WebinarService.getWebinars();
-  const data = response.data;
-  return (
-    <div className={styles.container}>
-      <Image
-        className={styles.leftSide}
-        src={"/chair.png"}
-        width={1500}
-        height={1500}
-        alt={"error"}
-        priority
-      />
-      <div className={styles.rightSide}>
-        <div className={styles.webinarsScroller}>
-          {data?.map((item: any, index: any) => {
-            return (
-              <Webinar
-                key={index}
-                heading={item.title}
-                description={item.description}
-                time={item.date}
-                type={item.status}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+	try {
+		const response = await WebinarService.getWebinars();
+		const data = response.data;
+		return (
+			<div className={styles.container}>
+				<Image
+					className={styles.leftSide}
+					src={"/chair.png"}
+					width={1500}
+					height={1500}
+					alt={"error"}
+					priority
+				/>
+				<div className={styles.rightSide}>
+					<div className={styles.webinarsScroller}>
+						{data?.map((item: any, index: any) => {
+							return (
+								<Webinar
+									key={index}
+									heading={item.title}
+									description={item.description}
+									time={item.date}
+									type={item.status}
+								/>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		);
+	} catch {
+		return <></>;
+	}
 };
